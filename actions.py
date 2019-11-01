13b4b531d9cd0c397c6272098b2b9d50820eeadc
Anirudha1995 <shirolkaranirudha@gmail.com>
2019-11-01 11:18:22 +0530

    Horoscope_bot

---
+++ actions.py
@@ -0,0 +1,51 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+#
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+import requests
+
+from rasa_core_sdk.events import SlotSet
+
+import logging
+logger = logging.getLogger(__name__)
+
+
+ 
+#
+class ActionGreet(Action):
+	def name(self):
+        	return "action_greet"
+     
+	def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+		dispatcher.utter_message("Hello! Tell me your Horoscope Sign")
+		return []
+
+class ActionTodaysHoroscope(Action):
+	def name(self):
+		return "action_todays_horoscope"
+    
+	def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+		user_horoscope_sign = tracker.get_slot('horoscope')
+		base_url = "http://horoscope-api.herokuapp.com/horoscope/{day}/{sign}"
+		url = base_url.format(**{'day': "today", 'sign': user_horoscope_sign})
+		#http://horoscope-api.herokuapp.com/horoscope/today/capricorn
+		res = requests.get(url, 'html.parser')
+		todays_horoscope = res.json()['horoscope']
+		response = "Your today's horoscope:\n{}".format(todays_horoscope)
+		dispatcher.utter_message(response)
+		return [SlotSet("horoscope", user_horoscope_sign)]
+
+class ActionGoodbye(Action):
+	def name(self):
+        	return "action_goodbye"
+     
+	def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+		
+
+		dispatcher.utter_message("Bye ! c u soon")
+		return []
+
